de32fcaaf3cbd03097166298efc396a15249122b
antbak_recipes <antbak_recipes@pinafore.dreamhost.com>
2018-07-23 18:00:17 -0700

    Create repo on server

---
+++ passenger_wsgi.py
@@ -0,0 +1,9 @@
+import sys, os
+INTERP = "/home/antbak_recipes/recipes.anthonywbaker.com/recipes_env/bin/python"
+#INTERP is present twice so that the new Python interpreter knows the actual executable path
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+from app import app as application
+
+# Helpful links to deploy:
+# https://help.dreamhost.com/hc/en-us/articles/215769548-Passenger-and-Python-WSGI
+# Note that I copied the all files from the bakera81/recipes repo to recipes.anthonywbaker.com
---
+++ static/js/test.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+$(".sub").hide();
+// $(".sub").css("display", "");
+$("#row1col1").click(function(){
+$(".sub").show();
+});
+});

55e056b26a84eb0a1b4a40ee8b18137752c4004a
Anthony Baker <bakera81@icloud.com>
2020-03-05 21:11:16 -0500

    WIP: contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import 'bulma/css/bulma.css'
+// import styled from "@emotion/styled"
+
+import Layout from  "../components/layout"
+import PageTitle from  "../components/pagetitle"
+import Container from  "../components/container"
+import ContactBlock from  "../components/contactblock"
+
+export default () => (
+  <Layout>
+    <PageTitle title="Contact"/>
+    <div className="columns is-multiline is-centered">
+      <ContactBlock title="Electronic-mail" text="anthonywbaker1[at]gmail.com" slug=""/>
+      <ContactBlock title="LinkedIn" text="awbaker1" slug="https://www.linkedin.com/in/awbaker1/"/>
+      <ContactBlock title="Github" text="bakera81" slug="https://github.com/bakera81"/>
+    </div>
+  </Layout>
+)

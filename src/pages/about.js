a6de57bbee432a0621db949fef366219d732b1ee
Anthony Baker <bakera81@icloud.com>
2020-03-14 17:09:08 -0400

    WIP: shuffle

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,10 +6,14 @@ import Layout from "../components/layout"
 import PageTitle from  "../components/pagetitle"
 import P from "../components/paragraph"
 
+const aboutMe = [
+  "Designer.", "Communicator.", "Data scientist.", "Process enthusiast.", "Growth marketer.", "Product manager.",
+]
+
 export default () => (
   <Layout>
     <PageTitle>About</PageTitle>
-    <P>Designer. Communicator. Data scientist. Process enthusiast. Growth marketer. Product manager. [shuffle]</P>
+    <P>{aboutMe.map(item => (item + " "))} [shuffle]</P>
     <hr css={{width: `100px;`, marginLeft: `auto;`, marginRight: `0;`}}/>
     <P>Creative writing & computer science (bachelor's): 2011-2015.</P>
     <P>Technology, innovation, and education (master's): 2015-2016.</P>

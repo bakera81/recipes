9b491db1abd86b7d816ae20cffc335e31ba0e3b2
Anthony Baker <bakera81@icloud.com>
2020-03-14 17:03:34 -0400

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,8 @@ import P from "../components/paragraph"
 export default () => (
   <Layout>
     <PageTitle>About</PageTitle>
+    <P>Designer. Communicator. Data scientist. Process enthusiast. Growth marketer. Product manager. [shuffle]</P>
+    <hr css={{width: `100px;`, marginLeft: `auto;`, marginRight: `0;`}}/>
     <P>Creative writing & computer science (bachelor's): 2011-2015.</P>
     <P>Technology, innovation, and education (master's): 2015-2016.</P>
     <P>Product, data science, and growth @ <a href="https://www.datacamp.com">DataCamp</a>: 2016-present.</P>

a6db70294374f73120b1daf4893ef368354af07a
Anthony Baker <bakera81@icloud.com>
2020-04-18 11:05:17 -0400

    update this site project description

--- src/pages/projects/awb.js
+++ src/pages/projects/awb.js
@@ -19,7 +19,12 @@ export default () => (
   <Layout>
     <Project imgRight title={frontmatter.title} imgSrc={projectImage}>
       <P style={{textAlign: `left`}}>I built this site with <a href="">Gatsby</a>, <a href="">Emotion</a>, and <a href="">Bulma</a>.</P>
-      <P style={{textAlign: `left`}}>I decided to deliberately make this site feel a bit offputting: The fonts are unpleasant to read and mismatched (Microsoft Sans Serif circa Windows 98 and a New York Times knockoff), text is right aligned, black font and whitespace is juxtaposed with bursts of color from classic Renaissance art.</P>
+      <P style={{textAlign: `left`}}>
+        I decided to deliberately make this site feel a bit offputting: The fonts are unpleasant to read and mismatched (Microsoft Sans Serif circa Windows 98 and a New York Times knockoff—stalwart fonts of digital and print), text is right aligned, black font and whitespace is juxtaposed with bursts of color from classic Renaissance art.
+      </P>
+      <P style={{textAlign: `left`}}>
+        Hopefully it says something about my fondness for irreverence and my distrust of settling for "normal."
+      </P>
     </Project>
   </Layout>
 )

00e3a3ff8208fffda9026813aec2651909cd412f
Anthony Baker <bakera81@icloud.com>
2020-04-18 10:24:47 -0400

    update contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,8 @@ export default () => (
   <Layout>
     <PageTitle>Contact</PageTitle>
     <P><b>Electronic-mail:</b> anthonywbaker1[at]gmail.com</P>
-    <P><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/awbaker1/">https://www.linkedin.com/in/awbaker1/</a></P>
-    <P><b>Github:</b>  <a href="https://github.com/bakera81">https://github.com/bakera81</a></P>
+    <P><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/awbaker1/">linkedin.com/in/awbaker1/</a></P>
+    <P><b>Github:</b>  <a href="https://github.com/bakera81">github.com/bakera81</a></P>
+    <P><b>Twitter:</b> <a href="https://twitter.com/AddieBundren81">twitter.com/AddieBundren81</a></P>
   </Layout>
 )

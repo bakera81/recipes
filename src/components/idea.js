c1ee15c9b46a190408cc64309fb9b83fcd3a985c
Anthony Baker <bakera81@icloud.com>
2020-04-20 22:40:47 -0400

    refactor MdContainer

--- src/components/idea.js
+++ src/components/idea.js
@@ -1,28 +1,21 @@
 import React from "react"
 import styled from "@emotion/styled"
 
+import renderHastToReact from "../helpers/renderHastToReact"
+
+import MdContainer from "../components/markdowncontainer"
+
 // import {AnchorLink} from "gatsby-plugin-anchor-links"
-import hastToHyperscript from "hast-to-hyperscript";
+// import hastToHyperscript from "hast-to-hyperscript";
 // import GithubSlugger from "github-slugger"
 // var slugger = new GithubSlugger()
 var slugger = require('github-slugger').slug;
 
 
 
-const renderHastToReact = node => {
-    return hastToHyperscript(React.createElement, node);
-}
-
-const MdContainer = styled.div`
-  p {
-    font-family: "Microsoft Sans Serif";
-    font-smooth: never;
-    -webkit-font-smoothing: none;
-    font-weight: normal;
-    padding-top: 1em;
-    padding-bottom: 1em;
-  }
-`
+// const renderHastToReact = node => {
+//     return hastToHyperscript(React.createElement, node);
+// }
 
 const Heading = styled.h4`
   margin-bottom: 0 !important;
---
+++ src/components/markdowncontainer.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled"
+
+const MdContainer = styled.div`
+  p {
+    font-family: "Microsoft Sans Serif";
+    font-smooth: never;
+    -webkit-font-smoothing: none;
+    font-weight: normal;
+    padding-top: 1em;
+    padding-bottom: 1em;
+  }
+`
+
+export default MdContainer
